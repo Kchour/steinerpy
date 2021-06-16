6d032dbabdadb2843383899741defd7ed6933547
Aarushi Mehta <mehta.aaru20@gmail.com>
2021-06-16 11:08:17 +0530

    generic_algorithms: fix index error in reprioritize()

--- steinerpy/algorithms/unmerged.py
+++ steinerpy/algorithms/unmerged.py
@@ -4,7 +4,6 @@ from steinerpy.framework import Framework
 from .common import Common
 import steinerpy.config as cfg
 from steinerpy.library.animation import AnimateV2
-from steinerpy.library.search.generic_algorithms import GenericSearch
 from steinerpy.library.search.search_utils import reconstruct_path, CycleDetection
 from steinerpy.library.logger import MyLogger
 
